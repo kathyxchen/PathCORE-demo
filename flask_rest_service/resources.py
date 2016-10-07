2ce14a24695d4c8638aa98609bf61f6dff40b6c9
Kathy <katch@sas.upenn.edu>
2016-10-07 16:24:59 -0400

    initial test

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -5,6 +5,25 @@ from flask.ext.restful import reqparse
 from flask_rest_service import app, api, mongo
 from bson.objectid import ObjectId
 
+class Edge(restful.Resource):
+    def __init__(self, *args, **kwargs):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('pw1', type=str)
+        self.parser.add_argument('pw2', type=str)
+        super(Edge, self).__init__()
+
+    def get(self):
+        args = self.parser.parse_args()
+        if not args['pw1'] or not args['pw2']:
+            abort(400)
+        jo1 = json.loads(args['pw1'])
+        jo2 = json.loads(args['pw2'])
+        return {
+            'test': jo1,
+            'test2': jo2
+        }
+
+
 class Root(restful.Resource):
     def get(self):
         return {
