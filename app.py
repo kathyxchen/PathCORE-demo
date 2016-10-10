a1c67a2b9422cc278fa2b8f17c83c22d20c4f317
Kathy <katch@sas.upenn.edu>
2016-10-10 09:57:25 -0400

    new changes to add in some semblance of a front-end. lots of work to figure out paths, etc. in flask (todo)

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask_rest_service import app
+from flask import render_template
+
+@app.route('/test')
+def test():
+	return render_template("index.html", title="test")
+
 app.run(debug=True)
 
