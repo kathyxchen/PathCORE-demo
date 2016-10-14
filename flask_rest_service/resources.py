704ba1f2e1ccf94b3714ad972140dd5c0f84866b
Kathy <katch@sas.upenn.edu>
2016-10-14 17:30:15 -0400

    setting up the basic network viz

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -3,20 +3,43 @@ from flask import request, abort
 from flask.ext import restful
 from flask.ext.restful import reqparse
 from flask_rest_service import app, api, mongo
+from webargs import fields, validate
+from webargs.flaskparser import use_args, parser
 from bson.objectid import ObjectId
 
 class Edge(restful.Resource):
-    def get(self, pw1, pw2):
+    def get(self, pw1, pw2, etype):
         pw1_obj = mongo.db.pathways.find_one_or_404({'_id': pw1})
         pw2_obj = mongo.db.pathways.find_one_or_404({'_id': pw2})
+
         return {
-            'Pathway 1': pw1_obj['pathway'],
-            'Pathway 2': pw2_obj['pathway']
+            'pathway 1': pw1_obj['pathway'],
+            'pathway 2': pw2_obj['pathway'],
+            'type': etype
         }
 
+class EdgeDir(Edge):
+    def get(self, pw1, pw2):
+        return super(EdgeDir, self).get(pw1, pw2, "direct")
+
+class EdgeInv(Edge):
+    def get(self, pw1, pw2):
+        return super(EdgeInv, self).get(pw1, pw2, "inverse")
+
 class Network(restful.Resource):
-    def get(self):
-        pass
+
+    args = {
+        "pw1": fields.Str(required=True),
+        "pw2": fields.Str(required=True),
+        "etype": fields.Str(required=True)
+    }
+
+    @use_args(args)
+    def get(self, args):
+        print args["pw1"]
+        print args["pw2"]
+        print args["etype"]
+        return {}
 
 class Root(restful.Resource):
     def get(self):
@@ -25,5 +48,7 @@ class Root(restful.Resource):
             'mongo': str(mongo.db)
         }
 
-api.add_resource(Edge, '/edge/<ObjectId:pw1>+<ObjectId:pw2>')
+api.add_resource(Network, '/network')
+api.add_resource(EdgeDir, '/edge/direct/<ObjectId:pw1>+<ObjectId:pw2>')
+api.add_resource(EdgeInv, '/edge/inverse/<ObjectId:pw1>+<ObjectId:pw2>')
 api.add_resource(Root, '/')
