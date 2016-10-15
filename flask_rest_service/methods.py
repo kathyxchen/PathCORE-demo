de4ee60b78813bcd5dfc35054a4f18eb3e4373a6
Kathy <katch@sas.upenn.edu>
2016-10-15 13:00:16 -0400

    odds ratio output json, i think the unannotated case has a bug

--- flask_rest_service/methods.py
+++ flask_rest_service/methods.py
@@ -5,36 +5,108 @@ DIR = 1
 MODEL_NODES = 300
 
 class PathwayData:
-
-    def __init__(self, pw_id, pw_name):
+    """Pathway information is dependent on the edge
+       the pathway is in.
+    """
+    def __init__(self, db, pw_id, pw_name):
+        self.db = db
     	self.id = pw_id
         self.name = pw_name
-    	self.pos_gene_counts = {}
-    	self.neg_gene_counts = {}
-        self.general_gene_counts = {}
+        # annotated: is the gene already known to be
+        # associated with this pathway by KEGG/GO?
+        # ALSO: account for node-specific crosstalk removal.
+        self.annotated_pos_genes = {}
+        self.annotated_neg_genes = {}
+        self.not_annotated_pos_genes = {}
+        self.not_annotated_neg_genes = {}
 
-    def update_gene_counts(self, gene_id, is_positive):
+    def bulk_update_gene_counts(self, node_pos_genes, node_neg_genes, annotated_info):
+        if annotated_info:
+            annotated_pos = annotated_info["pos_genes"]
+            annotated_neg = annotated_info["neg_genes"]
+            for gene_id in node_pos_genes:
+                if gene_id in annotated_pos:
+                    self.single_update_gene_counts(gene_id, True, True)
+                else:
+                    self.single_update_gene_counts(gene_id, True, False)
+            for gene_id in node_neg_genes:
+                if gene_id in annotated_neg:
+                    self.single_update_gene_counts(gene_id, False, True)
+                else:
+                    self.single_update_gene_counts(gene_id, False, False)
+
+    def single_update_gene_counts(self, gene_id, is_positive, is_annotated):
     	update = None
-    	if is_positive:
-    	    update = self.pos_gene_counts
-    	else:
-    	    update = self.neg_gene_counts
+    	if is_positive and is_annotated:
+    	    update = self.annotated_pos_genes
+        elif is_positive and not is_annotated:
+    	    update = self.not_annotated_pos_genes
+        elif not is_positive and is_annotated:
+            update = self.annotated_neg_genes
+        else:
+            update = self.not_annotated_neg_genes
     	if gene_id in update:
-    	    update += 1
+    	    update[gene_id] += 1
     	else:
     	    update[gene_id] = 1
-            if gene_id in self.general_gene_counts:
-                self.general_gene_counts[gene_id] += 1
-            else:
-                self.general_gene_counts[gene_id] = 1
 
 class EdgeData:
 
-    def __init__(self, pw0, pw1):
-    	self.edge = (pw0, pw1)
-    	self.shared_genes = {}
-    	self.odds_ratio_genes_side = {}
-    	self.odds_ratio_genes_any = {}
+    def __init__(self, db, pw1, pw2, in_num_nodes):
+    	self.db = db
+        self.edge = (pw1, pw2)
+        self.in_num_nodes = in_num_nodes
+        self.annotated_genes = {}
+        self.not_annotated_genes = {}
+
+        self.odds_ratios_genes = {}
+
+    def compute_odds_ratios(self):
+        # ignore gene pos/neg presence:
+        pw1 = self.edge[0]
+        pw2 = self.edge[1]
+        self._update_gene_counts_dict(self.annotated_genes, pw1.annotated_pos_genes)
+        self._update_gene_counts_dict(self.annotated_genes, pw1.annotated_neg_genes)
+        self._update_gene_counts_dict(self.not_annotated_genes, pw1.not_annotated_pos_genes)
+        self._update_gene_counts_dict(self.not_annotated_genes, pw1.not_annotated_neg_genes)
+
+        self._update_gene_counts_dict(self.annotated_genes, pw2.annotated_pos_genes)
+        self._update_gene_counts_dict(self.annotated_genes, pw2.annotated_neg_genes)
+        self._update_gene_counts_dict(self.not_annotated_genes, pw2.not_annotated_pos_genes)
+        self._update_gene_counts_dict(self.not_annotated_genes, pw2.not_annotated_pos_genes)
+
+        num_networks = len(self.db.network_edges.distinct("network"))
+        all_nodes = float(num_networks * MODEL_NODES)
+        or_denominator = self.in_num_nodes/all_nodes
+        or_keys = set(self.annotated_genes.keys()) | set(self.not_annotated_genes.keys())
+        for gene_id in or_keys:
+            annotated_or = 0.0
+            unannotated_or = 0.0
+            weighted_or = 0.0
+            look_for_gene = {"$or": [{"pos_genes": gene_id}, {"neg_genes": gene_id}]}
+            # over all networks, even if edge isn't present in a network.
+            gene_in_num_nodes = float(self.db.network_nodes.find(look_for_gene).count())
+
+            if gene_id in self.annotated_genes:
+                annotated_or = self.annotated_genes[gene_id]
+            if gene_id in self.not_annotated_genes:
+                unannotated_or = self.not_annotated_genes[gene_id]
+
+            if gene_id in self.annotated_genes and gene_id in self.not_annotated_genes:
+                total_gene_count = float(annotated_or + unannotated_or)
+                weighted_or = (annotated_or/total_gene_count)*total_gene_count
+
+            annotated_or = (annotated_or/gene_in_num_nodes)/or_denominator
+            unannotated_or = (unannotated_or/gene_in_num_nodes)/or_denominator
+            weighted_or = (weighted_or/gene_in_num_nodes)/or_denominator
+            self.odds_ratios_genes[gene_id] = (annotated_or, weighted_or, unannotated_or)
+
+    def _update_gene_counts_dict(self, gene_counts, to_add):
+        for gene, count in to_add.iteritems():
+            if gene not in gene_counts:
+                gene_counts[gene] = 0
+            gene_counts[gene] += count
+        return gene_counts
 
 class InteractionModel:
 
@@ -61,17 +133,54 @@ class InteractionModel:
         # Likely duplicates.
         oid1 = self.get_pw_id(pw1_name)["_id"]
         oid2 = self.get_pw_id(pw2_name)["_id"]
+        account_for_naming = {"$in": [[oid1, oid2], [oid2, oid1]]}
         # eventually, might need to have a 'filtered' flag in the DB
         # re: permutation analysis
         print oid1
         print oid2
         print etype
-        edge_in_networks = self.db.network_edges.find({"edge": [oid1, oid2], "type": etype})
-        return edge_in_networks
-        # for edge in edge_in_networks:
 
+        edge_in_networks = self.db.network_edges.find({"edge": account_for_naming,
+                                                       "type": etype})
+        # return edge_in_networks
 
+        pw1 = PathwayData(self.db, oid1, pw1_name)
+        pw2 = PathwayData(self.db, oid2, pw2_name)
+        edge_in_num_nodes = 0
+        # collect gene count information for both pathways.
+        for edge in edge_in_networks:
+            network = edge["network"]
+            nodes = edge["nodes"]
+            edge_in_num_nodes += len(nodes)
+            nodes_info = self.db.network_nodes.find(
+                {"network": network, "node": {"$in": nodes}})
+            for node in nodes_info:
+                pos_genes = node["pos_genes"]
+                neg_genes = node["neg_genes"]
+                node_pw1_def = self.db.network_node_pathways.find_one(
+                    {"network": network, "node": node["node"], "pathway": oid1})
+                node_pw2_def = self.db.network_node_pathways.find_one(
+                    {"network": network, "node": node["node"], "pathway": oid2})
 
+                pw1.bulk_update_gene_counts(pos_genes, neg_genes, node_pw1_def)
+                pw2.bulk_update_gene_counts(pos_genes, neg_genes, node_pw2_def)
+        aggregate_edge = EdgeData(self.db, pw1, pw2, edge_in_num_nodes)
+        aggregate_edge.compute_odds_ratios()
+        collect_annotated = []
+        collect_weighted = []
+        collect_unannotated = []
+        for gene_id, (annotated, weighted, unannotated) in aggregate_edge.odds_ratios_genes.iteritems():
+            gene_name = self.db.genes.find_one({"_id": gene_id})["gene"]
+            if weighted != 0:
+                collect_weighted.append((gene_name, weighted))
+            elif unannotated > 1:
+                collect_unannotated.append((gene_name, unannotated))
+            elif annotated > 1:
+                collect_annotated.append((gene_name, annotated))
+        collect_annotated.sort(key=lambda tup: tup[1])
+        collect_weighted.sort(key=lambda tup: tup[1])
+        collect_unannotated.sort(key=lambda tup: tup[1])
+        return {"annotated": collect_annotated, "weighted": collect_weighted, "unannotated": collect_unannotated}
 
 '''
 
