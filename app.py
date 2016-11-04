6333aeb298c89d1ce5d43476d881c8a55eeb1792
kchen17 <katch@seas.upenn.edu>
2016-11-03 20:43:14 -0400

    heatmaps have been set up for the edge pages

--- app.py
+++ app.py
@@ -15,15 +15,28 @@ def heatmap_test():
 def edge_inverse(edge_pws):
     pw1, pw2 = edge_pws.split("&")
     edge_info = mongo.db.ppin_edge_data.find_one({"edge": [pw1, pw2], "etype": -1})
+    edge_info["most_metadata"] = get_sample_metadata(edge_info["most_expressed_samples"])
+    edge_info["least_metadata"] = get_sample_metadata(edge_info["least_expressed_samples"])
     return render_template("test.html", edge_info=dumps(edge_info))
 
 @app.route("/edge/direct/<edge_pws>")
 def edge_direct(edge_pws):
     pw1, pw2 = edge_pws.split("&")   
     edge_info = mongo.db.ppin_edge_data.find_one({"edge": [pw1, pw2], "etype": 1})
+    edge_info["most_metadata"] = get_sample_metadata(edge_info["most_expressed_samples"])
+    edge_info["least_metadata"] = get_sample_metadata(edge_info["least_expressed_samples"])
     return render_template("test.html", edge_info=dumps(edge_info))
-    #analysis = methods.InteractionModel(mongo.db).get_edge_info(pw1, pw2, 1)
-    #return render_template("edge.html", pw1=pw1, pw2=pw2, etype=1, analysis=analysis)
+
+def get_sample_metadata(sample_names):
+    metadata = {}
+    for sample in sample_names:
+        info = mongo.db.sample_annotations.find_one({"CEL file": sample})
+        del info["_id"]
+        del info["CEL file"]
+        del info["sample_id"]
+        info.pop("Strain", None)
+        metadata[sample] = dumps(info)
+    return metadata
 
 app.run(debug=True)
 
